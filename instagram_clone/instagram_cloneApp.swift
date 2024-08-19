bc15c6906138b7765c4d4b34e06be79805ce7883
ZT <zinthantwin1997@gmail.com>
2024-08-19 15:45:51 +0630

    Initial Commit

---
+++ instagram_clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram_clone
+//
+//  Created by ကင်ဇို on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
