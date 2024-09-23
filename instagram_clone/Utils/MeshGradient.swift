e0aa7a06be59db8cb43a75a634beaa0ddd6cb1cf
ZT <zinthantwin1997@gmail.com>
2024-09-23 15:12:23 +0630

    wip gradient

---
+++ instagram_clone/Utils/MeshGradient.swift
@@ -0,0 +1,18 @@
+//
+//  MeshGradient.swift
+//  instagram_clone
+//
+//  Created by ကင်ဇို on 23/09/2024.
+//
+
+import SwiftUI
+
+struct MeshGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeshGradient()
+}
